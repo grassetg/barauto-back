38bc45c30277e1d9a77523aedb33b45449d5a566
Guillaume Grasset <guillaume.grasset@epita.fr>
2020-11-19 14:21:59 +0100

    Handle cors for the frontend

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const indexRouter = require('./routes/router');
 
 app.use(express.json())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
+
 // Create a redirection.
 app.use('/', indexRouter)
 
